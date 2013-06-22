6e84786a2d7b3852b4031e333ca84ac466a3cd51
John Minter <jrminter@gmail.com>
2013-06-22 00:36:14 -0400

    updated DTSA-2 std info

--- dtsa2/stds/stds.py
+++ dtsa2/stds/stds.py
@@ -1,4 +1,11 @@
 # -*- coding: utf-8 -*-
+import os
+import sys
+import shutil
+
+# Local system configuration options
+gitHome=os.environ['GIT_HOME']
+pyReptDir = gitHome + "/OSImageAnalysis/dtsa2/stds/stds Results/"
 
 def defineStd(elms,qty,name,density=None):
 	c=epq.Composition(map(element,elms),qty,name)
@@ -7,10 +14,11 @@ def defineStd(elms,qty,name,density=None):
 	Database.addStandard(c)
 
 defineStd(("Zn",),(1.000000,),"Zn standard",7.100000)
-defineStd(("Cu",),(1.000000,),"Cu",8.290000)
+defineStd(("Cu",),(1.000000,),"Cu",8.960000)
 defineStd(("Cu",),(1.000000,),"Cu standard",8.960000)
 defineStd(("Fe",),(1.000000,),"Fe standard",7.880000)
 defineStd(("Mn",),(1.000000,),"Mn standard",7.410000)
+defineStd(("Ni",),(1.000000,),"Ni",8.908000)
 defineStd(("O","Mg","Si","Ca","Fe",),(0.423667,0.088465,0.253817,0.110563,0.112087,),"K411",5.000000)
 defineStd(("O","Mg","Al","Si","Ca","Fe",),(0.427580,0.116567,0.049062,0.211982,0.108990,0.077420,),"K412")
 # SRM 484. Data from Carpenter, Microanalysis Tools AMAS 2009
@@ -20,6 +28,10 @@ defineStd(("Cu","Au",),(0.1983,0.8015,),"SRM-484", 15.6808)
 # Data from http://www.nist.gov/srm/upload/SP260-73.PDF
 # density from http://www.ask.com/answers/337173021/what-is-the-density-of-316-stainless-steel
 defineStd(("Fe","Cr","Ni","Mn","Cu","C","Mo","P","S",),(0.53414,0.2543,0.2091,0.0014,0.0004,0.0003,0.0002,0.0001,0.00006,),"SS-316", 7.99)
+
+# clean up cruft
+shutil.rmtree(pyReptDir)
+print "Done!"
 
 
 
