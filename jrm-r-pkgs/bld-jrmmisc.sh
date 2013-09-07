e598d1b09ff7a1ba72c7ba180629857881900239
John Minter <jrminter@rochester.rr.com>
2013-09-06 21:05:21 -0400

    updated our EDAX detector XML files to have 4096 channels

--- jrm-r-pkgs/bld-jrmmisc.sh
+++ jrm-r-pkgs/bld-jrmmisc.sh
@@ -2,8 +2,8 @@
 cd ~/git/jrm-r-pkgs
 R CMD build jrmmisc
 R CMD check jrmmisc
-R CMD INSTALL ./jrmmisc_1.0.1.tar.gz
-rm ./jrmmisc_1.0.1.tar.gz
+R CMD INSTALL ./jrmmisc*.tar.gz
+rm ./jrmmisc*.tar.gz
 rm -rf jrmmisc.Rcheck
 
 
