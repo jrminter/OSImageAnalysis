67b787c56f67557058f82ea4afa4b9564abc967f
John Minter <jrminter@rochester.rr.com>
2014-05-17 00:44:42 -0400

    better version of dtsa2/other-macros/createCustomSDD.py

--- dtsa2/other-macros/createCustomSDD.py
+++ dtsa2/other-macros/createCustomSDD.py
@@ -21,35 +21,99 @@ dName = "cubeSDD"
 nChan = 2048
 eVpC  =   10.0
 resMn =  128.0
-toA   =   35.0   # take-off angle, deg
-sToDd =   54.56  # sample to detector distance, mm
-dA    =   80.00  # detector area, sq mm
-opWD  =    5.00  # optimum working distance, mm
-alLay =   10.0   # Al layer, nm
+zOff  =  100.0
 
-sR2 = math.sqrt(2.0)
+def createTempDetector(rDet, dName, nChan, eVpC, zOff, resMn):
+  """createTempDetector(rDet, dName, nChan, eVpC, zOff, resMn)
+  Create a temporary detector with name (dName) with (nChan) channels and
+  (eVpC) ev/ch and (zOff) zero offset with resMn resolution"""
+  
+  # define a couple of helper functions...
+  def copyNumericProp(nD, oD, prop):
+    """copyNumericProp(nD, oD, prop)
+    Copy numeric prop from old detector (oD) to new detector (nD)"""
+    odp = oD.getDetectorProperties().getProperties()
+    ndp = nD.getDetectorProperties().getProperties()
+    np  = odp.getNumericProperty(prop)
+    ndp.setNumericProperty(prop, np)
 
-refDet = findDetector("FEI FIB620 EDAX-RTEM")
+  def copyArrayProp(nD, oD, prop):
+    """copyArrayProp(nD, oD, prop)
+    Copy array prop from old detector (oD) to new detector (nD)"""
+    odp = oD.getDetectorProperties().getProperties()
+    ndp = nD.getDetectorProperties().getProperties()
+    np  = odp.getArrayProperty(prop)
+    ndp.setArrayProperty(prop, np)
 
-# Create a new detector
-dd=epq.Detector.EDSDetector.createSDDDetector(nChan, eVpC, resMn)
-dd.getDetectorProperties().setName(dName)
-dp=dd.getDetectorProperties().getProperties()
-# Take-off -> 25 deg, sample-det distant -> 9.22 mm, optimal working distance-> 2.2 mm
-dp.setDetectorPosition(toA*3.1415926/180.0, 0.0, sToDd*1e-03, opWD*1.0e-03)
-# Set the detector to point at 225 degrees
-dp.setArrayProperty(epq.SpectrumProperties.DetectorOrientation,[-1.0*sR2, -1.0*sR2, 0.0])
-dp.setNumericProperty(epq.SpectrumProperties.Elevation, toA)
-# Replace the gold layer with a Aluminum layer
-dp.setNumericProperty(epq.SpectrumProperties.GoldLayer, 0.0)
-dp.setNumericProperty(epq.SpectrumProperties.AluminumLayer, 10.0)
-dp.setNumericProperty(epq.SpectrumProperties.DetectorArea, dA)
-dp.setNumericProperty(epq.SpectrumProperties.MoxtekWindow, 3.3)
 
-# sa = dp.getNumericProperty(epq.SpectrumProperties.SolidAngle)
+  # Create a new temporary detector
+  tD=epq.Detector.EDSDetector.createSDDDetector(nChan, eVpC, resMn)
+  tD.getDetectorProperties().setName(dName)
+  tdp = tD.getDetectorProperties().getProperties()
+  tdp.setNumericProperty(epq.SpectrumProperties.EnergyOffset, zOff)
+  
+  copyArrayProp(tD, rDet, epq.SpectrumProperties.DetectorPosition)
+  copyArrayProp(tD, rDet, epq.SpectrumProperties.DetectorOrientation)
+
+  copyNumericProp(tD, rDet, epq.SpectrumProperties.Elevation)
+  copyNumericProp(tD, rDet, epq.SpectrumProperties.AluminumLayer)
+  copyNumericProp(tD, rDet, epq.SpectrumProperties.DetectorArea)
+  copyNumericProp(tD, rDet, epq.SpectrumProperties.DetectorDistance)
+  copyNumericProp(tD, rDet, epq.SpectrumProperties.SolidAngle)
+  copyNumericProp(tD, rDet, epq.SpectrumProperties.MoxtekWindow)
+  
+  return tD
+
+# start clean
+DataManager.clearSpectrumList()
+
+rD = findDetector("Oxford XMaxN 80")
+theTD = createTempDetector(rD, dName, nChan, eVpC, zOff, resMn)
+print(theTD.toString())
+dp = theTD.getDetectorProperties().getProperties()
 dd = dp.getNumericProperty(epq.SpectrumProperties.DetectorDistance)
 print(dd)
+rDp = rD.getDetectorProperties().getProperties()
+dd = rDp.getNumericProperty(epq.SpectrumProperties.DetectorDistance)
+print(dd)
 
+# let's use it to do something
+cu  = material("Cu", density=8.96)
+sim = simulate(cu, det=theTD, keV=20, dose=100, withPoisson=True)
+display(sim)
+
+sf = epq.SpectrumFitter8(theTD, cu, sim)
+# print(dir(sf))
+rois = sf.getROIS()
+print(rois)
+
+# print(epq.FromSI.keV(rois.highEnergy()-rois.lowEnergy())/3.0)
+
+pr = sim.getProperties()
+# print(dir(pr))
+props = pr.getDetector().getCalibration().getProperties()
+# print(props)
+coeffs = [props.getNumericWithDefault(epq.SpectrumProperties.EnergyOffset, 0.0),
+props.getNumericWithDefault(epq.SpectrumProperties.EnergyScale, 10.0),
+props.getNumericWithDefault(epq.SpectrumProperties.EnergyQuadratic, 0.0),
+0.0,
+0.0]
+print(coeffs)
+sf.setEnergyScale(epq.SpectrumFitter8.EnergyScaleFunction(coeffs, 6))
+sf.setResolution(epq.SpectrumFitter8.FanoNoiseWidth(6.0));
+sf.setMultiLineset(sf.buildWeighted(rois));
+sf.compute()
+fitSpectrum = sf.getBestFit()
+display(fitSpectrum)
+
+els = sf.getElementSpectrum(epq.Element.Cu)
+display(els)
+
+bss = sf.getBremsstrahlungSpectrum()
+display(bss)
+
+bfp = sf.getBestFitParameters()
+print(bfp)
 
 
 # clean up cruft
