d68a2c6cc7d328017d28fb2c840bea5ce2f8a665
John Minter <jrminter@gmail.com>
2014-07-26 10:40:42 -0400

    added python scripts to ana .dm3 files of clumped grains using Fiji

---
+++ ImageJ/macros/anaClumpedAgX.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+# anaClumpedAgX.py
+#  Modifications
+#   Date      Who  Ver                       What
+# ----------  --- ------  -------------------------------------------------
+# 2014-07-26  JRM 0.1.00  initial prototype development. Note that Fiji 
+#                         can read .dm3 files directly. N.B. This version
+#                         adds the environment variable 'RPT_ROOT'
+import os
+import glob
+import time
+
+def ensureDir(d):
+  """ensureDir(d)
+  Check if the directory, d, exists, and if not create it."""
+  if not os.path.exists(d):
+    os.makedirs(d)
+
+imgDir  = os.environ['IMG_ROOT']
+rptDir  = os.environ['RPT_ROOT']
+relImg  = "/test/clumpAgX"
+sampID  = "qm-03966-KJL-031"
+nmPerPx = 1.213
+minCirc = 0.5
+minSize = 1.0e1
+maxSize = 1.0e6
+
+# set some strings
+strScale = "distance=1 known=%.3f pixel=1 unit=nm global" % nmPerPx
+strAna   = "size=%g-%g circularity=%.3f-1.00 show=[Overlay Outlines] display exclude clear include" % (minSize, maxSize, minCirc)
+
+sImgPath = imgDir + relImg + "/" + sampID + "/"
+sRptPath = rptDir + "/" + sampID + "/"
+ensureDir(sRptPath)
+
+query = sImgPath + "*.dm3"
+lFiles = glob.glob(query)
+for fi in lFiles:
+  imp = ImagePlus(fi)
+  strName = os.path.basename(fi)
+  strName = strName.split('.')[0]
+  imp.setTitle(strName)
+  imp.show()
+  IJ.run("Enhance Contrast", "saturated=0.35")
+  IJ.run("8-bit")
+  IJ.run("Threshold", "method=Default white")
+  IJ.run("Watershed")
+  IJ.run("Set Measurements...", "area fit shape feret's display redirect=None decimal=5")
+  IJ.run("Set Scale...", strScale)
+  
+  IJ.run("Analyze Particles...", strAna)
+  rFiName = strName + ".csv"
+  rFiPath = sRptPath + rFiName
+  IJ.saveAs("Results", rFiPath)
+  imp.changes = False
+  imp.close()
