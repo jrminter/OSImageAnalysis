6010eb41d45eb5eb88e3f1db0b41ac658b279ee8
John Minter <john.r.minter@kodak.com>
2014-08-18 16:59:23 -0400

    used Gohlke code to read FIB 620 TIF metadata

--- py3/testReadTifMetadata.py
+++ py3/testReadTifMetadata.py
@@ -1,18 +1,42 @@
 # -*- coding: utf-8 -*-
 """
+testReadTifMetadata.py
+
+Read the metadata from an FEI FIB 620 TIF image using the Christoph Gohlke
+tifffile code. A work in progress.
+
 Created on Mon Aug 18 15:47:33 2014
 
-@author: l837410
+@author: John Minter
 """
 
 import os
 import tifffile
+import matplotlib.pyplot as plt
+import skimage.io as io
+
 home=os.environ['HOME']
 imgRoot=os.environ['IMG_ROOT']
 
 relImg  = "/test/suite/"
-# n.b. 17.56 and 17.57 - this gets it wrong both 24.3889
+# n.b. 17.56 and 17.57 scale X and Y by analySIS Five.
 fName = 'fib620.tif'
 filePath = imgRoot + relImg + fName
 
 print(filePath)
+tif = tifffile.TiffFile(filePath)
+im = tif[0].asarray()
+
+io.imshow(im, cmap=plt.cm.gray)
+plt.show()
+
+for page in tif:
+  for tag in page.tags.values():
+    t = tag.name, tag.value
+    # what I want is in tag 34680
+    if(tag.name == '34680'):
+      myStr = tag.value.decode()
+      ary = myStr.split('\r\n')
+      for i in range(len(ary)):
+        print(ary[i])
+      
