691f8f41dd4080ccab1282e7814dde37dcde7710
John Minter <jrminter@gmail.com>
2014-10-11 15:56:42 -0400

    cleaned up macros

---
+++ ImageJ/macros/py/commonImports.py
@@ -0,0 +1,10 @@
+import os
+import glob
+import time
+
+from ij import IJ
+from ij import ImagePlus
+from ij import WindowManager
+from io.scif.img import ImgOpener
+
+import jmFijiGen as jmg
--- ImageJ/macros/py/makeSimpleMontage_.py
+++ ImageJ/macros/py/makeSimpleMontage_.py
@@ -11,12 +11,6 @@ from org.python.core import codecs
 codecs.setDefaultEncoding('utf-8')
 
 import os
-import glob
-import time
-from ij import IJ
-from ij import ImagePlus
-from ij import WindowManager
-from io.scif.img import ImgOpener
 import jmFijiGen as jmg
 
 gitDir = os.environ['GIT_HOME']
@@ -24,11 +18,12 @@ relImg = "/OSImageAnalysis/images/map/png"
 inDir  = gitDir + relImg
 lNames  = ["2-OK","3-CuL","6-PK","7-PdL","8-AgL","9-ROI"]
 
+#        sz   w-px  um
 lCal = [1.45, 512, -6]
+#       x0  y0  w   h
 lCr  = [200,30,200,220]
 
 impMont = jmg.makeMontage(lNames, 3, 2, inDir, lCal=lCal, lCr=lCr, sca=1.0)
-# impMont = jmg.calibAZtecImage(impMont, 1.45, 512)
 impMont.show()
 
 
