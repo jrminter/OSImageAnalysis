621a1a8e97b1e18110917df3ebc81a3c9cd8f3a4
John Minter <john.r.minter@kodak.com>
2014-10-15 10:20:33 -0400

    added a simple script to calibrate images (mainly from AZtec)

---
+++ ImageJ/macros/py/calibImage_.py
@@ -0,0 +1,26 @@
+# calibImage_.py
+#
+# Apply a calibration to an image from the full width
+#
+#  Modifications
+#   Date      Who  Ver                       What
+# ----------  --- ------  -------------------------------------------------
+# 2014-10-15  JRM 0.1.00  Get'er done
+
+from org.python.core import codecs
+codecs.setDefaultEncoding('utf-8')
+
+import os
+import jmFijiGen as jmg
+from ij import IJ
+from ij import WindowManager
+
+# full width in microns
+fw = 14.5
+
+impCal = WindowManager.getCurrentImage()
+impCal = jmg.calibImage(impCal, fw, units=-6)
+impCal.show()
+
+
+
