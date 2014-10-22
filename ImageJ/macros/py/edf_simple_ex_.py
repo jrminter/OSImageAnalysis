a2b3fcc57385a66e871298c5a9664b4c591aff10
John Minter <jrminter@gmail.com>
2014-10-21 20:07:30 -0400

    updated edf_simple_ex_.py

--- ImageJ/macros/py/edf_simple_ex_.py
+++ ImageJ/macros/py/edf_simple_ex_.py
@@ -5,10 +5,17 @@
 #   Date      Who  Ver                       What
 # ----------  --- ------  -------------------------------------------------
 # 2014-10-19  JRM 0.1.00  copied from source with minor adaptations
-#                         TO DO: figure out expert mode from exemplar
+# 2014-10-21  JRM 0.1.10  added timing loop and contrast stretching
+#                         added a crop boundary delta in case alignment
+#                         is not perfect                         
 from org.python.core import codecs
 codecs.setDefaultEncoding('utf-8')
 
+from ij import IJ
+from ij import ImagePlus
+from ij import WindowManager
+
+import time
 import jarray
 from edfgui import BasicDialog
  
@@ -20,10 +27,13 @@ here need to check conditions of the image, it should not be less than
 - 2 slices
 '''
 # should first par be width?
-imagesize = jarray.array([imp.getHeight(), imp.getHeight()], 'i')
+w = imp.getWidth()
+h = imp.getHeight()
+imagesize = jarray.array([w, h], 'i')
 color = imp.getType() == ImagePlus.COLOR_RGB
 dl = BasicDialog(imagesize, color, False)
 #  "quality='1' topology='0' show-view='on' show-topology='off'"
+delta = 0
 quality = 1
 topology = 0
 showview = True
@@ -33,3 +43,10 @@ dl.parameters.setTopologySettings(topology)
 dl.parameters.show3dView = showview 
 dl.parameters.showTopology = showtopology
 dl.process()
+while (WindowManager.getWindow("Output") is None ):
+  time.sleep(1)
+impEDF = WindowManager.getCurrentImage()
+impEDF.show()
+IJ.makeRectangle(delta, delta, w-2*delta, h-2*delta)
+IJ.run("Crop")
+IJ.run("Enhance Contrast", "saturated=0.0")
