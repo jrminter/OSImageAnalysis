1a696420aca62c963a3af0d2a073b583e56ed741
John Minter <jrminter@gmail.com>
2014-11-01 22:35:32 -0400

    frustrating Win7 in headless mode...

---
+++ ImageJ/macros/py/headless/HelloWorld.py
@@ -0,0 +1,8 @@
+# HelloWord.py
+#
+# load and crop an image in headless mode
+print("Hello World")
+
+
+
+
---
+++ ImageJ/macros/py/headless/testHeadlessPC.cmd
@@ -0,0 +1,9 @@
+ImageJ-win64.exe --java-home "C:\Apps\Fiji.app.win64\java\win64\jdk1.6.0_24\jre" --headless --jython --console --mem=1000m HelloWorld.py
+
+REM "C:\Apps\Fiji.app.win32\ImageJ-win32.exe" --java-home "C:\Apps\Fiji.app.win32\java\win32\jdk1.6.0_24\jre" --headless --jython --console --mem=1000m HelloWorld.py
+
+
+
+
+pause
+
