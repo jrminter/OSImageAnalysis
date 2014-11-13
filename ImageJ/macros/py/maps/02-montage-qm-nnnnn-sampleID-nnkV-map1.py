7d2bd62afa8ff39b6a346e2f22ed04a38381059c
John Minter <john.r.minter@kodak.com>
2014-11-13 13:38:05 -0500

    added option to scale a map montage

--- ImageJ/macros/py/maps/02-montage-qm-nnnnn-sampleID-nnkV-map1.py
+++ ImageJ/macros/py/maps/02-montage-qm-nnnnn-sampleID-nnkV-map1.py
@@ -7,7 +7,8 @@
 # 2014-11-07  JRM 0.1.00  Montage maps created with 01-Prep script
 #                         this version tries to use standard directories
 # 2014-11-07  JRM 0.1.10  Added a bTestPaths flag to test loading images
-# 2014-11-10  JRM 0.0.11  Be sure to set title 
+# 2014-11-10  JRM 0.1.11  Be sure to set title 
+# 2014-11-13  JRM 0.1.12  Option to scale
 
 from org.python.core import codecs
 codecs.setDefaultEncoding('utf-8')
@@ -16,7 +17,12 @@ import os
 import jmFijiGen as jmg
 from ij import IJ
 
+# check that paths are correct by loading ROI image
 bTestPaths = False
+# scale the image by scaFac
+bScale     = True
+scaFac     = 0.5
+
 homDir     = os.environ['HOME']
 edsDir     = os.environ['EDS_ROOT']
 ePrjDir    = "QM14-nn-nnA-Client"
@@ -57,8 +63,20 @@ if bTestPaths == True:
 else:
   jmg.ensureDir(outDir)
   impMontFull = jmg.makeMontage(lName, nCol, nRow, inDir, lCal=lCal, sca=1.0)
-  impMontFull.setTitle(sampID + "-" + mapID)
-  impMontFull.show()
-  IJ.saveAs(impMontFull, "PNG", outPth)
+  if bScale:
+    newW = round(scaFac * impMontFull.getWidth())
+    newH = round(scaFac * impMontFull.getHeight())
+    newN = sampID + "-" + mapID
+    strSca = "x=%g y=%g width=%d height=%d interpolation=Bicubic average create title=%s" % (scaFac, scaFac, newW, newH, newN)
 
-
+    IJ.run(impMontFull, "Scale...", strSca )
+    impMontFull.changes=False
+    impMontFull.close()
+    impOut = IJ.getImage()
+  else:
+    impOut = impMontFull
+    
+  impOut.setTitle(sampID + "-" + mapID)
+  impOut.show()
+  
+  IJ.saveAs(impOut, "PNG", outPth)
