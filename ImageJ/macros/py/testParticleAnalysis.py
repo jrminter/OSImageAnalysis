875bd83cd5627a5521840e7e6ce82a6c0e7f59be
John Minter <john.r.minter@kodak.com>
2014-11-19 09:38:41 -0500

    added testParticleAnalysis.py

--- ImageJ/macros/py/testParticleAnalysis.py
+++ ImageJ/macros/py/testParticleAnalysis.py
@@ -39,58 +39,95 @@ strAna   = "size=%g-%g circularity=%.3f-1.00 show=[Overlay Outlines] display exc
 sImgPath = imgDir + relImg + "/" + sampID + "/"
 sRptPath = rptDir + "/" + sampID + "/"
 jmg.ensureDir(sRptPath)
+sRptCsvPath = sRptPath + sampID + ".csv"
+sRptImgPath = sRptPath + "png/"
+jmg.ensureDir(sRptImgPath)
 
 query = sImgPath + "*.dm3"
 lFiles = glob.glob(query)
+# create empty output vectors for results
+imgOut = []
+ecdOut = []
+cirOut = []
+rndOut = []
+solOut = []
+arOut  = []
+gmOut  = []
 i = 0
 for fi in lFiles:
-  if i < 1:
-    orig = ImagePlus(fi)
-    strName = os.path.basename(fi)
-    strName = strName.split('.')[0]
-    orig.setTitle(strName)
-    orig.show()
-    iZero = jmg.findI0(orig, maxSearchFrac=0.5, chAvg=5)
-    print(iZero)
-    imp = orig.duplicate()
-    imp.setTitle("work")
-    imp.show()
-    IJ.run(imp, "Enhance Contrast", "saturated=0.35")
-    IJ.run(imp, "8-bit", "")
-    IJ.run(imp, "Threshold", "method=Default white")
-    IJ.run(imp, "Watershed", "")
-    imp.show()
-    strSetMeas = "area modal min perimeter fit shape feret's redirect=%s decimal=3" % strName
-    print(strSetMeas)
-    IJ.run(imp, "Set Measurements...", strSetMeas)
-    strAna = "size=%g-%g circularity=%.3f-1.00 show=[Overlay Outlines] display exclude clear include add" % (minSize, maxSize, minCirc)
-    IJ.run(imp, "Analyze Particles...", strAna)
-    rt = ResultsTable.getResultsTable()
-    nMeas = rt.getCounter()
-    print(nMeas)
-    aSqNm = rt.getColumn(ResultsTable.AREA)
-    ecdCol = rt.getFreeColumn("ECD.nm")
-    print(ecdCol)
-    rt.setHeading(ecdCol, "ECD.nm") 
-    # ecdNm = []
-    for j in range(len(aSqNm)):
-      ecd = 2.0*sqrt(aSqNm[j]/3.1415926)
-      rt.setValue(ecdCol, j, 17) 
-      # rt.addValue(ecdCol, ecd)
-      # ecdNm.append(ecd)
-    rt.updateResults() 
-    # gMode = rt.getColumn(ResultsTable.MODE) 
-    if bClose:
-      imp.changes = False
-      imp.close()
-
-    # Now draw particles into the original image
-    rm = RoiManager.getInstance()
-    ra = rm.getRoisAsArray()
-    for r in ra:
-      orig.setRoi(r)
-      strStroke = "  stroke=%s width=%g" % (col, wid)
-      IJ.run(orig, "Properties... ", strStroke )
-      IJ.run(orig, "Add Selection...", "")
+  orig = ImagePlus(fi)
+  strName = os.path.basename(fi)
+  strName = strName.split('.')[0]
+  orig.setTitle(strName)
+  orig.show()
+  iZero = jmg.findI0(orig, maxSearchFrac=0.5, chAvg=5)
+  print(iZero)
+  imp = orig.duplicate()
+  imp.setTitle("work")
+  imp.show()
+  IJ.run(imp, "Enhance Contrast", "saturated=0.35")
+  IJ.run(imp, "8-bit", "")
+  IJ.run(imp, "Threshold", "method=Default white")
+  IJ.run(imp, "Watershed", "")
+  imp.show()
+  strSetMeas = "area modal min perimeter fit shape feret's redirect=%s decimal=3" % strName
+  print(strSetMeas)
+  IJ.run(imp, "Set Measurements...", strSetMeas)
+  strAna = "size=%g-%g circularity=%.3f-1.00 show=[Overlay Outlines] display exclude clear include add" % (minSize, maxSize, minCirc)
+  IJ.run(imp, "Analyze Particles...", strAna)
+  rt = ResultsTable.getResultsTable()
+  nMeas = rt.getCounter()
+  print(nMeas)
+  aSqNm = rt.getColumn(ResultsTable.AREA)
+  circ  = rt.getColumn(ResultsTable.CIRCULARITY)
+  rnd   = rt.getColumn(ResultsTable.ROUNDNESS)
+  sol   = rt.getColumn(ResultsTable.SOLIDITY)
+  ar    = rt.getColumn(ResultsTable.ASPECT_RATIO)
+  gMode = rt.getColumn(ResultsTable.MODE) 
     
+  for j in range(len(aSqNm)):
+    ecd = 2.0*sqrt(aSqNm[j]/3.1415926)
+    imgOut.append(i+1)
+    ecdOut.append(ecd)
+    cirOut.append(circ[j])
+    rndOut.append(rnd[j])
+    solOut.append(sol[j])
+    arOut.append(ar[j])
+    gmOut.append(gMode[j])
+      
+  # rt.reset()  
+  # rt.updateResults() 
+  if bClose:
+    imp.changes = False
+    imp.close()
+      
+  # Now draw particles into the original image
+  rm = RoiManager.getInstance()
+  ra = rm.getRoisAsArray()
+  for r in ra:
+    orig.setRoi(r)
+    strStroke = "  stroke=%s width=%g" % (col, wid)
+    IJ.run(orig, "Properties... ", strStroke )
+    IJ.run(orig, "Add Selection...", "")
+    orig.killRoi()
+
+  rm.close() 
+  outPth = sRptImgPath + strName + ".png"
+  # burn a scale bar and save the image
+  IJ.run(orig, "RGB Color", "")
+  IJ.run(orig, "Add Scale Bar", "width=100 height=6 font=28 color=Green location=[Lower Right] bold")
+  IJ.saveAs(orig, "PNG", outPth)
+  orig.changes = False
+  orig.close()
+  
   i += 1 
+
+# prepare and write the output file
+f=open(sRptCsvPath, 'w')
+strLine = 'ing, ecd.nm, circ, rnd, a.r, solidity, gray.mode\n'
+f.write(strLine)
+for i in range(len(imgOut)):
+  strLine = "%d, %.2f, %.4f, %.4f, %.4f, %.4f, %.2f\n" % (imgOut[i], ecdOut[i], cirOut[i], rndOut[i], arOut[i], solOut[i], gmOut[i] )
+  f.write(strLine)
+
+f.close()
