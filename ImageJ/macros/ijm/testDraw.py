531b8eb6b3d14e766238663e32e7bd25b418825c
John Minter <jrminter@gmail.com>
2014-11-22 19:27:43 -0500

    updated the prototype label image script

--- ImageJ/macros/ijm/testDraw.py
+++ ImageJ/macros/ijm/testDraw.py
@@ -13,27 +13,41 @@
 from org.python.core import codecs
 codecs.setDefaultEncoding('utf-8')
 
+
+import os
+
 from java.awt import Color
 from ij import IJ, ImagePlus
 from ij.gui import TextRoi, Overlay
 
-def labelImage(imp, label, x, y, font=24, col=Color.WHITE):
-  tr = TextRoi(10, 0, label)
-  tr.setColor(col)
-  tr.setFont("SanSerif", font, 1) 
-  tr.setJustification(TextRoi.CENTER)
-  tr.setAntialiased(True)
+def labelImage(imp, lLab, lX, lY, font=24, col=Color.WHITE):
+  l = len(lLab)
+  # make a copy
+  res = imp.duplicate()
+  # let's create an array of text rois
   ol = Overlay()
-  ol.add(tr)
-  imp.setOverlay(ol)
-  imp.updateAndRepaintWindow()
-  imp.show()
-  return imp
+  for i in range(l):
+    tr = TextRoi(lX[i], lY[i]-(font/2), lLab[i])
+    tr.setColor(col)
+    tr.setFont("SanSerif", font, 1) 
+    tr.setJustification(TextRoi.CENTER)
+    tr.setAntialiased(True)
+    ol.add(tr)
+  res.show()
+  res.setOverlay(ol)
+  res.updateAndRepaintWindow()
+  return res
 
+homDir = os.environ['HOME']
+imgPath = homDir + '/git/OSImageAnalysis/images/blobs.gif'
+raw = IJ.openImage(imgPath)
+raw.show()
 
-raw = IJ.getImage()
+lX   = [ 74,  90, 180 ]
+lY   = [ 41, 160, 180 ]
+lLab = ["1", "2", "3" ]
 
-labelImage(raw, "R", 3, 10) 
+labelImage(raw, lLab, lX, lY) 
 
 
 
