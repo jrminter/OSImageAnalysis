e80be8fcddff4db4c4d6c9ee42b2a0c876cede2c
John Minter <jrminter@gmail.com>
2014-12-19 22:46:30 -0500

    testBilateralFilter

---
+++ ImageJ/macros/py/testBilateral.py
@@ -0,0 +1,31 @@
+# testBilateral.py
+#
+# see https://github.com/fiji/VIB/blob/master/src/main/java/Bilateral_Filter.java
+#
+#  Modifications
+#   Date      Who  Ver                       What
+# ----------  --- ------  -------------------------------------------------
+# 2014-12-19  JRM 0.1.00  initial prototype development.
+#                         
+from org.python.core import codecs
+codecs.setDefaultEncoding('utf-8')
+import os
+from ij import IJ
+from ij.process import ImageStatistics
+from ij.measure import Measurements
+from vib import BilateralFilter
+
+spatialRadius = 10
+factor = 20
+
+
+edsDir  = os.environ['EDS_ROOT']
+imgPth = edsDir + "/Oxford/QM14-04-03A5-English/reports/qm-04206-Pd8-1-FIB-2/qm-04206-Pd8-1-FIB-2-7kV-map1/tif/O-K.tif"
+imp = IJ.openImage(imgPth)
+stats = imp.getStatistics(Measurements.MIN_MAX)
+imp.setDisplayRange(stats.min, stats.max)
+rangeRadius = factor*(stats.max - stats.min)
+IJ.run(imp, "8-bit", "")
+imp = BilateralFilter.filter(imp, spatialRadius, rangeRadius)
+IJ.run(imp, "Enhance Contrast", "saturated=0.35")
+imp.show()
