028c6c743ef48a0bca8bbc141a45569e024fc443
John Minter <jrminter@gmail.com>
2014-12-23 11:44:54 -0500

    more noise reduction tests

--- ImageJ/macros/py/testNoiseReduction.py
+++ ImageJ/macros/py/testNoiseReduction.py
@@ -12,6 +12,7 @@ import os
 from ij import IJ
 from ij.process import ImageStatistics
 from ij.measure import Measurements
+from ij.plugin.filter import RankFilters
 
 import jmFijiGen as jmg
 
@@ -81,9 +82,42 @@ def clipNoisePixMapImage(imp, no=2):
   ret.setTitle(name)
   return ret  
 
+def rankFilterMapImage(imp, radius=1.5, no=2):
+  """clipNoisePixMapImage(imp, no=2)
+  Clips noise pixels from an X-ray map image (typically a 16 bit gray image). 
+  First, it sets the display range to a noise offset to max and removes the noise
+  pixels (to get rid of isolated pixels), then converts to an 8 bit image that spans
+  0 to 255 and returns an 8 bit gray scale. This is ready for a  hueLUT. It peforms
+  this on a duplicate imp and returns the resultant imp. To the best of my understanding,
+  this is how Oxford treats their maps w/o a 3x3 smooth. 
+  Inputs:
+  imp - the input ImagePlus object
+  no  - the noise offset, default = 2, to remove noise pixels
+  Returns:
+  ret - an ImapePlus for the 8-bit, scaled, filtered image
+  """
+  stats = imp.getStatistics(Measurements.MIN_MAX)
+  imp.setDisplayRange(0, stats.max)
+  ret = imp.duplicate()
+  ip = ret.getProcessor()
+  data = ip.getPixels()
+  l = len(data)
+  for i in range(l):
+    val = data[i]
+    if val < no:
+      data[i] = 0
+  rf = RankFilters()
+  rf.rank(ret.getProcessor(), radius, RankFilters.MEDIAN)
+  IJ.run(ret, "8-bit", "")
+  name = imp.getShortTitle()
+  stats = ret.getStatistics(Measurements.MIN_MAX)
+  ret.setDisplayRange(0, stats.max)
+  ret.setTitle(name)
+  return ret 
+
 
 edsDir  = os.environ['EDS_ROOT']
 imgPth = edsDir + "/Oxford/QM14-04-03A5-English/reports/qm-04206-Pd8-1-FIB-2/qm-04206-Pd8-1-FIB-2-7kV-map1/tif/Cu-L.tif"
 imp = IJ.openImage(imgPth)
-ret = jmg.smoothMapImage(imp, no=3)
+ret = rankFilterMapImage(imp, radius=1.0, no=2)
 ret.show()
