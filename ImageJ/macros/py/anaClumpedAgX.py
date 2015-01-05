e4fef0c6f79480023bfa1b16f9ce4452d4d691eb
John Minter <john.r.minter@kodak.com>
2015-01-05 09:43:43 -0500

    updated anaClumpedAgX.py: added img and part no and fixed scale bar. v. 0.2.10

--- ImageJ/macros/py/anaClumpedAgX.py
+++ ImageJ/macros/py/anaClumpedAgX.py
@@ -8,6 +8,7 @@
 # 2014-09-30  JRM 0.1.10  Moved some code to jmFijiGen and edit for ImageJ2
 # 2015-01-04  JRM 0.2.00  Changed to call anaParticlesWatershed() from
 #                         jmFijiGen and to compute contrast
+# 2015-01-05  JRM 0.2.10  Added image and particle number and fixed scale bar
 from org.python.core import codecs
 codecs.setDefaultEncoding('utf-8')
 import os
@@ -30,6 +31,13 @@ minCirc = 0.5
 minSize = 1.0e1
 maxSize = 1.0e6
 
+barW    =    100  # bar width, nm
+barH    =      6  # bar height, pts
+barF    =     28  # bar font, pts
+barC    = "Black" # bar color
+
+strBar = "width=%g height=%g font=%g color=%s location=[Lower Right] bold" % (barW, barH, barF, barC)
+
 sImgPath = imgDir + relImg + "/" + sampID + "/"
 sRptPath = rptDir + "/" + sampID + "/"
 jmg.ensureDir(sRptPath)
@@ -39,6 +47,7 @@ jmg.ensureDir(sRptImgPath)
 
 # create empty output vectors for results
 imgOut = []
+parOut = []
 ecdOut = []
 conOut = []
 cirOut = []
@@ -48,7 +57,9 @@ arOut  = []
 
 query = sImgPath + "*.dm3"
 lFiles = glob.glob(query)
+i = 0
 for fi in lFiles:
+  i += 1
   orig = ImagePlus(fi)
   strName = os.path.basename(fi)
   strName = strName.split('.')[0]
@@ -56,7 +67,11 @@ for fi in lFiles:
   l = len(lStr)
   strNum = lStr[l-1]
   orig.setTitle(strNum)
-  # orig.show()
+  if i == 1:
+    # a hack to get the scale bars to work reliably
+    foo = orig.duplicate()
+    IJ.run(foo, "RGB Color", "")
+    IJ.run(foo, "Add Scale Bar", strBar)
   iZero = jmg.findI0(orig, maxSearchFrac=0.5, chAvg=5)
   # print(iZero)
   rt = jmg.anaParticlesWatershed(orig, minPx=30)
@@ -75,7 +90,8 @@ for fi in lFiles:
   lRound  = rt.getColumn(rt.getColumnIndex("Round"))
   lSolid  = rt.getColumn(rt.getColumnIndex("Solidity"))
   for j in range(len(lArea)):
-    imgOut.append(strNum)
+    imgOut.append(i)
+    parOut.append(j+1)
     ecd = 2.0*sqrt(lArea[j]/3.1415926)
     ecdOut.append(ecd)
     con = lMode[j]/iZero
@@ -89,7 +105,7 @@ for fi in lFiles:
   outPth = sRptImgPath + strName + ".png"
   # burn a scale bar and save the image
   IJ.run(orig, "RGB Color", "")
-  IJ.run(orig, "Add Scale Bar", "width=100 height=6 font=28 color=Black location=[Lower Right] bold")
+  IJ.run(orig, "Add Scale Bar", strBar)
   IJ.saveAs(orig, "PNG", outPth)
   orig.changes = False
   orig.close()
@@ -98,10 +114,10 @@ for fi in lFiles:
 
 # prepare the output file
 f=open(sRptCsvPath, 'w')
-strLine = 'img, ecd.nm, contrast, circ, a.r, round, solidity\n'
+strLine = 'img, part, ecd.nm, contrast, circ, a.r, round, solidity\n'
 f.write(strLine)
 for i in range(len(ecdOut)):
-  strLine = "%s, %.2f, %.4f, %.4f, %.4f, %.4f, %.4f\n" % (imgOut[i], ecdOut[i],  conOut[i], cirOut[i], arOut[i], rndOut[i], solOut[i] )
+  strLine = "%d, %d, %.2f, %.4f, %.4f, %.4f, %.4f, %.4f\n" % (imgOut[i], parOut[i], ecdOut[i],  conOut[i], cirOut[i], arOut[i], rndOut[i], solOut[i] )
   f.write(strLine)
 
 f.close()
