cd04089266759ca2bc4e7e24bdeca0fdb31bf489
John Minter <jrminter@gmail.com>
2015-12-31 18:27:34 -0500

    added skdemo py pkg

---
+++ skdemo/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='skdemo',
+      version='0.0.90.dev1',
+      description='Utility functions for skimage demos',
+      url='https://github.com/scikit-image/skimage-tutorials/tree/master/lectures/skdemo',
+      author='Many',
+      author_email='jrminter@gmail.com',
+      license='GPL-2',
+      packages=['skdemo'],
+      zip_safe=False)
