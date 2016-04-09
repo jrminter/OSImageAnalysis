9a17575b3bb8e40ce0ced5e472f2b6ed6bfb30c3
John Minter <jrminter@gmail.com>
2016-04-09 17:07:44 -0400

    added hyperspy example new interface

--- py3/snippets/circular_diffraction.py
+++ py3/snippets/circular_diffraction.py
@@ -1,19 +1,19 @@
 # Source: http://central.scipy.org/item/73/2/circular-diffraction
 
 # License: Creative Commons Zero (almost public domain) http://scpyce.org/cc0
-from numpy import *
+import numpy as np
 from scipy.special import j1
-from matplotlib.pyplot import *
+import matplotlib.pyplot as plt
 
 wave = 500e-9
-k = 2*pi/wave
+k = 2*np.pi/wave
 side = 1e-6
 points = 200
-dim = linspace(-side/2,side/2,points)
-ax,ay = meshgrid(dim,dim)
-r = sqrt(ax**2 + ay**2)
+dim = np.linspace(-side/2,side/2,points)
+ax,ay = np.meshgrid(dim,dim)
+r = np.sqrt(ax**2 + ay**2)
 area = (j1(k*r)/(k*r))**2
-imshow(area)
-hot()
-show()
+plt.imshow(area, cmap='viridis');
+# hot()
+plt.show();
 
