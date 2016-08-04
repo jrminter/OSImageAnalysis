030a34d35b7f161449f18daa123a836f5d0b7926
John Minter <jrminter@gmail.com>
2016-08-04 18:29:03 -0400

    wrote measureLengthWrapper.py

---
+++ ImageJ/macros/py/_Common/measureLengthWrapper.py
@@ -0,0 +1,33 @@
+# @string(label = "CSV File") csvPath
+# @boolean(label = "append") bAppend
+# @boolean(label = "debug", value=False) bDebug
+# @int(label = "line width", value=2) lw
+# @int(label = "offset", value=-30) offset
+# @int(label = "digits", value=-3) digits
+# @int(label = "font", value=18) font
+#
+# measureLengthWrapper.py
+#
+# Measure lengths in a series of image
+#
+#  Modifications
+#   Date      Who  Ver                       What
+# ----------  --- ------  -------------------------------------------------
+# 2016-08-04  JRM 0.1.00  Initial prototype.
+
+from org.python.core import codecs
+codecs.setDefaultEncoding('utf-8')
+
+import os
+import time
+import jmFijiGen as jmg
+from ij import IJ, Prefs, WindowManager
+from java.awt import Color, Font
+import csv
+from ij.io import FileSaver, DirectoryChooser
+import ConfigParser
+
+imp = IJ.getImage()
+jmg.measureFeatureLength(imp, lw, csvPath, bAppend, offset, digits,
+                         font, Color.YELLOW, Color.WHITE, bDebug)
+
