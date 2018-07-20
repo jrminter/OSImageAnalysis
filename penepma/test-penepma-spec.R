4e5e3b162e29d4d331fcd5b778041c75c57c100f
John Minter <jrminter@gmail.com>
2018-07-20 14:23:57 -0400

    a bit more cleanup

--- penepma/test-penepma-spec.R
+++ penepma/test-penepma-spec.R
@@ -1,14 +1,22 @@
-library(rpenepma)
+# Plot a penepma spectrum and optionally convert to .msa format
+# 2018-07-20 requires the rpenepma package from github
+#
+#
+# set paths and kV
 
 gitDir <- Sys.getenv('GIT_HOME')
 relDir <- "OSImageAnalysis/penepma"
+spcTi  <- "pe-spect-01"
+e0     <- 30.0 # kV
+doMsa  <- FALSE # Usually only want to write the final one...
 
+# shold not need to change below here...
 
-inFil <- sprintf("%s/%s/%s", gitDir, relDir,
-                 "pe-spect-01.dat" )
+library(rpenepma)
 
-outFil <- sprintf("%s/%s/%s", gitDir, relDir,
-                  "pe-spect-01.msa" )
+inFil <- sprintf("%s/%s/%s", gitDir, relDir, "pe-spect-01.dat" )
+
+outFil <- sprintf("%s/%s/%s.msa", gitDir, relDir, spcTi )
 
 df <- penepma_read_raw_data(inFil)
 rownames(df) <- c()
@@ -16,13 +24,16 @@ print(head(df))
 print(summary(df))
 
 
-plt <- penepma_plot_spectrum(inFil, "pe-spect-01")
+plt <- penepma_plot_spectrum(inFil, spcTi)
 plt <- plt +
        # coord_trans(y= "sqrt") +
        NULL
 print(plt)
 
-plt <- penepma_plot_spectrum_logy(inFil, "pe-spect-01")
+plt <- penepma_plot_spectrum_logy(inFil, spcTi)
 print(plt)
 
-# penepma_to_msa(inFil, outFil, 30.0, "pe-spect-01")
+if (doMsa == TRUE) {
+  penepma_to_msa(inFil, outFil, e0, spcTi)
+}
+
