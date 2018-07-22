946cc969e83b60616c33651863d3adcc092cafd9
John Minter <jrminter@gmail.com>
2018-07-21 23:34:05 -0400

    edited Ir 250 nm on Ag 150 nm on Silica at 30 kV

---
+++ penepma/temp.bat
@@ -0,0 +1,5 @@
+C:
+cd "C:\Userdata\Penepma12\Penepma"
+Penepma < "20-nm-C-coated-EagleXG.in"
+
+pause
--- penepma/test-penepma-spec.R
+++ penepma/test-penepma-spec.R
@@ -1,5 +1,5 @@
 # Plot a penepma spectrum and optionally convert to .msa format
-# 2018-07-20 requires the rpenepma package from github
+# 2018-07-21 requires the rpenepma package from github
 #
 #
 # set paths and kV
@@ -7,11 +7,14 @@
 gitDir <- Sys.getenv('GIT_HOME')
 relDir <- "OSImageAnalysis/penepma"
 spcTi  <- "pe-spect-01"
-outTi <-  "penepma-Ir-500nm-Ag-250nm-Silica"
-e0     <- 30.0 # kV
+outTi  <-  "20-nm-C-coated-EagleXG"
+e0     <-   15.0 # kV
 doMsa  <- FALSE # Usually only want to write the final one...
+pWid   <- 1024
+pHt    <-  768
+ptSz   <-   12
 
-# shold not need to change below here...
+# should not need to change below here...
 
 library(rpenepma)
 
@@ -26,13 +29,37 @@ print(summary(df))
 
 
 plt <- penepma_plot_spectrum(inFil, spcTi)
-plt <- plt +
-       # coord_trans(y= "sqrt") +
-       NULL
 print(plt)
+
+
+outPng <- sprintf("%s/%s/%s-linear.png", gitDir, relDir, spcTi )
+
+png(outPng, width = pWid, height = pHt, units = "px")
+
+plt <- plt + theme(axis.text=element_text(size=24),
+                   axis.title=element_text(size=24),
+                   plot.title = element_text(size=28, hjust = 0.5)) +
+  # coord_trans(y= "sqrt") +
+  NULL
+print(plt)
+dev.off()
+
 
 plt <- penepma_plot_spectrum_logy(inFil, spcTi)
 print(plt)
+
+outPng <- sprintf("%s/%s/%s-logY.png", gitDir, relDir, spcTi )
+
+
+png(outPng, width = pWid, height = pHt, units = "px")
+plt <- plt + theme(axis.text=element_text(size=24),
+                   axis.title=element_text(size=24),
+                   plot.title = element_text(size=28, hjust = 0.5)) +
+  # coord_trans(y= "sqrt") +
+  NULL
+print(plt)
+dev.off()
+
 
 if (doMsa == TRUE) {
   outFil <- sprintf("%s/%s/%s.msa", gitDir, relDir, outTi )
